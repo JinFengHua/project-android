3e85669ecd70432aea4410ddc347ad800eb0d31f
JinFengHua <66351642+JinFengHua@users.noreply.github.com>
2022-05-03 17:07:17 +0800

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/project_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/project_android/ExampleUnitTest.java
@@ -32,17 +32,10 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 
-    private String test = "[{\"courseId\":2,\"teacherId\":1,\"courseName\":\"云计算\",\"courseAvatar\":\"faceimages/face.jpg\",\"courseIntroduce\":\"云计算云计算云计算云计算云计算云计算\",\"courseCode\":\"647904\",\"teacher\":{\"teacherId\":1,\"adminId\":1,\"teacherAccount\":\"000001\",\"teacherPassword\":\"000000\",\"teacherName\":\"张老师\",\"teacherSex\":false,\"teacherPhone\":\"13137749525\",\"teacherEmail\":\"2116161338@qq.com\",\"teacherAvatar\":\"faceimages/face.jpg\",\"courses\":null}},{\"courseId\":4,\"teacherId\":1,\"courseName\":\"数据库\",\"courseAvatar\":\"faceimages/face.jpg\",\"courseIntroduce\":\"云计算云计算云计算云计算云计算云计算\",\"courseCode\":\"322989\",\"teacher\":{\"teacherId\":1,\"adminId\":1,\"teacherAccount\":\"000001\",\"teacherPassword\":\"000000\",\"teacherName\":\"张老师\",\"teacherSex\":false,\"teacherPhone\":\"13137749525\",\"teacherEmail\":\"2116161338@qq.com\",\"teacherAvatar\":\"faceimages/face.jpg\",\"courses\":null}},{\"courseId\":5,\"teacherId\":1,\"courseName\":\"计算机原理\",\"courseAvatar\":\"faceimages/face.jpg\",\"courseIntroduce\":\"云计算云计算云计算云计算云计算云计算\",\"courseCode\":\"353964\",\"teacher\":{\"teacherId\":1,\"adminId\":1,\"teacherAccount\":\"000001\",\"teacherPassword\":\"000000\",\"teacherName\":\"张老师\",\"teacherSex\":false,\"teacherPhone\":\"13137749525\",\"teacherEmail\":\"2116161338@qq.com\",\"teacherAvatar\":\"faceimages/face.jpg\",\"courses\":null}}]";
-
+    private String test = "[]";
     private Map<String,String> initMap(){
         Map<String,String> map = new HashMap<>();
-        map.put("name","江道宽");
-        map.put("account","081417158");
-        map.put("sex","true");
-        map.put("major","计科一班");
-        map.put("password","1234566778");
-        map.put("phone","13137749525");
-        map.put("email","2116161338@qq.com");
+       
         return map;
     }
 
